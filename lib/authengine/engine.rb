ed6d4b88294b716a5c15d0e2861fbafcb0343aa1
Les Nightingill <les@orphansofrwanda.org>
2011-04-09 06:20:36 -0700

    add an initializer to the engine spec to try to load static assets from the engine

--- lib/authengine/engine.rb
+++ lib/authengine/engine.rb
@@ -9,18 +9,18 @@ module Authengine
     # Config defaults
     config.widget_factory_name = "default factory name"
     config.mount_at = '/'
-    
+
     # Load rake tasks
     #rake_tasks do
       #load File.join(File.dirname(__FILE__), 'rails/railties/tasks.rake')
     #end
-    
+
     # Check the gem config
     initializer "check config" do |app|
       # make sure mount_at ends with trailing slash
       config.mount_at += '/'  unless config.mount_at.last == '/'
     end
-    
+
     initializer "static assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end
@@ -31,6 +31,11 @@ module Authengine
         include AuthorizedSystem
       end
     end
-    
+
+    # serve static assets directly from the engine
+    initializer "static assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
+
   end
 end
