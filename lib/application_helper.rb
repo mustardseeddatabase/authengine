329eedbcd100e07db77d8ec238877c75b4c6b4cb
Les Nightingill <les@orphansofrwanda.org>
2011-04-19 21:11:54 -0700

    refactoring

--- lib/application_helper.rb
+++ lib/application_helper.rb
@@ -12,7 +12,16 @@ module ApplicationHelper
   end
 
   def submit_or_return_to(return_path)
-    render :partial=>'authengine/shared/submit_or_return_to', :locals=>{:return_path=>return_path}
+    haml_tag :table, {:style => 'padding-top:30px'} do
+      haml_tag :tr do
+        haml_tag :td, {:width => '180px'} do
+          haml_tag :input, {:type => 'submit', :value => 'Save'}
+        end
+        haml_tag :td do
+          haml_tag :a, "Cancel", {:href => return_path}
+        end
+      end
+    end
   end
 
 private
