7e7dffb6764deefe9673e378c7536b2d8914f613
Les Nightingill <codehacker@comcast.net>
2012-04-29 18:51:01 -0700

    change to action_roles_controller 'on a hunch' since it's working in dev environment but not in production

--- app/controllers/authengine/action_roles_controller.rb
+++ app/controllers/authengine/action_roles_controller.rb
@@ -4,8 +4,10 @@ class Authengine::ActionRolesController < ApplicationController
   def update_all
     aa = ActionRole.all.group_by(&:role_id).inject({}){|hash,a| hash[a[0]]=a[1].collect(&:action_id); hash}
     params[:permission].each do |role_id,permissions| # role is the role name, permissions is a hash of controller/action names
+       role_id = role_id.to_i
        permissions.each do |action_id, val|
-         a = aa[role_id.to_i].nil? ? false : aa[role_id.to_i].include?(action_id.to_i) # because a new role, with no permissions granted, produces nil for aa[role_id.to_i]
+         action_id = action_id.to_i
+         a = aa[role_id].nil? ? false : aa[role_id].include?(action_id) # because a new role, with no permissions granted, produces nil for aa[role_id.to_i]
          if val=="1" && !a # a newly-checked checkbox
            ActionRole.new(:role_id=>role_id,:action_id=>action_id).save
          elsif val=="0" && a # a newly-unchecked checkbox
