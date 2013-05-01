df269864d2219fa082a12267ce3c0bf68f08d50f
Les Nightingill <codehacker@comcast.net>
2013-04-30 21:41:39 -0700

    odify

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302000835) do
+ActiveRecord::Schema.define(:version => 20130430043717) do
 
   create_table "action_roles", :force => true do |t|
     t.integer  "role_id",    :limit => 8
@@ -62,11 +62,10 @@ ActiveRecord::Schema.define(:version => 20130302000835) do
   end
 
   create_table "user_roles", :force => true do |t|
-    t.integer  "role_id",    :limit => 8,                                   :null => false
-    t.integer  "user_id",    :limit => 8,                                   :null => false
-    t.datetime "created_at",                                                :null => false
-    t.datetime "updated_at",                                                :null => false
-    t.string   "type",                    :default => "PersistentUserRole"
+    t.integer  "role_id",    :limit => 8, :null => false
+    t.integer  "user_id",    :limit => 8, :null => false
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
   end
 
   create_table "useractions", :force => true do |t|
